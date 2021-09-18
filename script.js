113c426122a1b03dce759ea1f92f4e7f37342c3a
emelda <emelda.perez@moringaschool.com>
2021-09-19 00:18:06 +0300

    Add variables.

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function orderPizza(){
     var name=$("input#name").val();
     var flavor=$("#flavor").val();
     var size=$("size").val();
+    var amount=$("#amount").val();
     var toppings=[];
 }
