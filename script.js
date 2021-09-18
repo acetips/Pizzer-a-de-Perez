2712bf8ef08eb28beb531abffe86180985b52db2
emelda <emelda.perez@moringaschool.com>
2021-09-19 00:15:48 +0300

    Create variables for collecting user input in the form.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Business Logic
+//Function for ordering a pizza
+function orderPizza(){
+    var name=$("input#name").val();
+    var flavor=$("#flavor").val();
+    var size=$("size").val();
+    var toppings=[];
+}
