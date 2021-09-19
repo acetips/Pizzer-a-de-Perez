ec52f0a8c4003f28bfc0ce5aca2841a727b4f0e2
emelda <emelda.perez@moringaschool.com>
2021-09-19 12:32:18 +0300

    crate a variable to hold the prices of various pizza flavors.

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ function orderPizza(){
     var size=$("size").val();
     var amount=$("#amount").val();
     var toppings=[];
+    
+    $.each($('input[name="toppings"]:checked'),
+        function(){
+            toppings.push($(this).val());
+        })
+    
+    var price;
 }
